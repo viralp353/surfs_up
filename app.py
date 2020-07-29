d1e96815de441ac64446ff29e5a651064636f2f6
viralp353 <65969608+viralp353@users.noreply.github.com>
2020-07-29 12:04:31 -0500

    station route

--- app.py
+++ app.py
@@ -44,4 +44,9 @@ def precipitation():
    precip = {date: prcp for date, prcp in precipitation}
    return jsonify(precip)
 
-#
+#create Stations Route
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
